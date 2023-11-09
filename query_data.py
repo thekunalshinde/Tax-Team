01b65f538d9de51f977cacb832e23fccd1d0f25e
isayhc <isayahculbertson@gmail.com>
2023-11-09 05:06:40 -0500

    turned the api call into a function

--- query_data.py
+++ query_data.py
@@ -1,60 +1,65 @@
-import requests
 import json
 import os
+import requests
 
-url = "https://api.vectara.io/v1/query"
+def vectara_api_call(query:str, corpus_id=1):
+    url = "https://api.vectara.io/v1/query"
 
-payload = json.dumps({
-  "query": [
-    {
-      "query": "What is the answer to the life, the universe, and everything?",
-      "start": 0,
-      "numResults": 10,
-      "contextConfig": {
-        "charsBefore": 30,
-        "charsAfter": 30,
-        "sentencesBefore": 3,
-        "sentencesAfter": 3,
-        "startTag": "<b>",
-        "endTag": "</b>"
-      },
-      "corpusKey": [
+    payload = json.dumps({
+      "query": [
         {
-          "customerId": os.environ['VECTARA_CUSTOMER_ID'],
-          "corpusId": 1,
-          "semantics": "DEFAULT",
-          "dim": [
+          "query": query,
+          "start": 0,
+          "numResults": 10,
+          "contextConfig": {
+            "charsBefore": 30,
+            "charsAfter": 30,
+            "sentencesBefore": 3,
+            "sentencesAfter": 3,
+            "startTag": "<b>",
+            "endTag": "</b>"
+          },
+          "corpusKey": [
             {
-              "name": "string",
-              "weight": 0
+              "customerId": os.environ['VECTARA_CUSTOMER_ID'],
+              "corpusId": corpus_id,
+              "semantics": "DEFAULT",
+              "dim": [
+                {
+                  "name": "string",
+                  "weight": 0
+                }
+              ],
+              "metadataFilter": "part.lang = 'eng'",
+              "lexicalInterpolationConfig": {
+                "lambda": 0
+              }
             }
           ],
-          "metadataFilter": "part.lang = 'eng'",
-          "lexicalInterpolationConfig": {
-            "lambda": 0
-          }
-        }
-      ],
-      "rerankingConfig": {
-        "rerankerId": 272725717
-      },
-      "summary": [
-        {
-          "summarizerPromptName": "string",
-          "maxSummarizedResults": 0,
-          "responseLang": "string"
+          "rerankingConfig": {
+            "rerankerId": 272725717
+          },
+          "summary": [
+            {
+              "summarizerPromptName": "string",
+              "maxSummarizedResults": 0,
+              "responseLang": "string"
+            }
+          ]
         }
       ]
+    })
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'x-api-key': os.environ['VECTARA_API_KEY'],
+      'customer-id': os.environ['VECTARA_CUSTOMER_ID']
     }
-  ]
-})
-headers = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json',
-  'x-api-key': os.environ['VECTARA_API_KEY'],
-  'customer-id': os.environ['VECTARA_CUSTOMER_ID']
-}
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response
 
-print(response.text)
+
+if __name__ == '__main__':
+    response = vectara_api_call(query="what does the universe contain")
+    print(response.text)
