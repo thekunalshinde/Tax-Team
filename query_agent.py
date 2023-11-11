54af84892e8298db345d8d719e68616b2d4ad6b4
Isayah Culbertson <isayahCulbertson@gmail.com>
2023-11-11 16:06:19 -0500

    Update query_agent.py
    fixed typo in UI

--- query_agent.py
+++ query_agent.py
@@ -147,7 +147,7 @@ st.title("💬 Chatbot")
 with st.sidebar:
     # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[View the source code](https://github.com/isayahc/Tax-Team/blob/streamlit/query_agent.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 if prompt := st.chat_input():
